32de5723aa12dcd1baa765d70d3d2ef17fb64822
Sp1r14ual <sp1r14ual@gmail.com>
2022-05-31 03:59:41 +0700

    Code Refactoing for Lab 3

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -1,71 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <cstddef>
+#include "tree.cpp"
 
 using namespace std;
-
-typedef struct tnode
-{
-  int field;
-  struct tnode *left;
-  struct tnode *right;
-} tnode;
-
-void treeprint_preorder(tnode *tree)
-{
-  if (tree != NULL)
-  {
-    cout << tree->field << " ";
-    treeprint_preorder(tree->left);
-    treeprint_preorder(tree->right);
-  }
-}
-
-void treeprint_inorder(tnode *tree)
-{
-  if (tree != NULL)
-  {
-    treeprint_inorder(tree->left);
-    cout << tree->field << " ";
-    treeprint_inorder(tree->right);
-  }
-}
-
-void treeprint_postorder(tnode *tree)
-{
-  if (tree != NULL)
-  {
-    treeprint_postorder(tree->left);
-    treeprint_postorder(tree->right);
-    cout << tree->field << " ";
-  }
-}
-
-tnode *addnode(int x, tnode *tree)
-{
-  if (tree == NULL)
-  {
-    tree = new tnode;
-    tree->field = x;
-    tree->left = NULL;
-    tree->right = NULL;
-  }
-  else if (x < tree->field)
-    tree->left = addnode(x, tree->left);
-  else
-    tree->right = addnode(x, tree->right);
-  return tree;
-}
-
-void freemem(tnode *tree)
-{
-  if (tree != NULL)
-  {
-    freemem(tree->left);
-    freemem(tree->right);
-    free(tree);
-  }
-}
 
 int path_length(tnode *tree, int value)
 {
